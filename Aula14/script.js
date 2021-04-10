8eba779ee0f27c0eaafacc7143e320b591921eac
matheuscassioli <matheuscassioli@hotmail.com>
2021-04-10 15:29:06 -0300

    subindo + atuzaliaçoes

--- Aula14/script.js
+++ Aula14/script.js
@@ -1,9 +1,19 @@
-function contar() {
+function Contar() {
     var inicio = document.getElementById("txtinicio")
     var fim = document.getElementById("txtfim")
     var passo = document.getElementById("txtpasso")
     var res = document.getElementById("res")
-    if (inicio.value.lenght == 0) {
+    var c = i
+
+    if (inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
         window.alert("Digite os números")
+    } else {
+        res.innerHTML = "CONTANDO:"
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        for (var c = i; c <= f; c += p) {
+            res.innerHTML += `${c}`
+        }
     }
 }
