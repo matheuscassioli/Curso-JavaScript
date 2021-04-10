8239069870c53c817f9da687ce71a8240174553f
matheuscassioli <matheuscassioli@hotmail.com>
2021-04-10 16:20:14 -0300

    atualizando novamente

--- Aula14/script.js
+++ Aula14/script.js
@@ -3,17 +3,29 @@ function Contar() {
     var fim = document.getElementById("txtfim")
     var passo = document.getElementById("txtpasso")
     var res = document.getElementById("res")
-    var c = i
 
     if (inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
+        res.innerHTML = "Impossivel contar"
         window.alert("Digite os números")
     } else {
         res.innerHTML = "CONTANDO:"
         var i = Number(inicio.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
-        for (var c = i; c <= f; c += p) {
-            res.innerHTML += ` ${ c   }  \u{1F57A}`
+        if (p <= 0) {
+            window.alert("Passo inválido, considerando PASSO 1")
+            p = 1
+        }
+        if (i < f) {
+            // contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c}  \u{1F57A}`
+            }
+        } else {
+            //contagem regressiva   
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F57A}`
+            }
         }
     }
 }
