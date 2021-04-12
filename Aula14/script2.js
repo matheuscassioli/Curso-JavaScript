7d2770b5fa2b3fbef1d135ca71e217754a8fb4e4
matheuscassioli <matheuscassioli@hotmail.com>
2021-04-12 18:25:28 -0300

    Atualizando c/ EX017

---
+++ Aula14/script2.js
@@ -0,0 +1,18 @@
+function Tabuada() {
+    var num = document.getElementById("txtn")
+    var tab = document.getElementById("seltab")
+    if (num.value.lenght == 0) {
+        window.alert("É Necessário digitar um número ")
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ""
+        while (c <= 10) {
+            var item = document.createElement("option")
+            item.innerText = `${n} x ${c} = ${n *c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
