807d4b3cb22a5884fff6d02ae105081c719ef40e
matheuscassioli <matheuscassioli@hotmail.com>
2021-04-13 18:17:38 -0300

    atualizando aula 15

---
+++ Aula15/vetornatela.js
@@ -0,0 +1,13 @@
+var valores = [5, 6, 2, 9]
+    //console.log(valores)
+    /*
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+*/
+
+for (let pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+
+}
