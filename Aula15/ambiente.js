6931e154498e7700b1d7f500138b8b61dd8d4120
matheuscassioli <matheuscassioli@hotmail.com>
2021-04-13 18:39:56 -0300

    descobrindo se existe valor desejado e seu indice

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -1,9 +1,16 @@
 let num = [5, 8, 2, 9, 3]
-num.push(10) // adiciona 10 pós ultimo vetor
+let pos = num.indexOf(3)
+    //num.push(10) // adiciona 10 pós ultimo vetor
 console.log(num)
-
-
 console.log(`Nosso vetor tem ${num.length} posições`) // .lenght sabe quantos vetores possui
-console.log(`Nosso vetor na posição 6 é o ${num[5]}`) // localiza elemento através da posição
+    //console.log(`Nosso vetor na posição 6 é o ${num[5]}`) // localiza elemento através da posição
 
-//num.sort() -- organiza vetores em ordem crescente
+
+if (pos == -1) { //quando não possui o valor solicitado, o js imprimi -1 
+    console.log(`O valor declarado não existe dentre os vetores`)
+} else {
+    console.log(`Existe o valor 3? ... Caso exista a posição é ${pos}`)
+}
+
+
+//  .sort() -- organiza vetores em ordem crescente
--- Aula15/vetornatela.js
+++ Aula15/vetornatela.js
@@ -5,9 +5,9 @@ console.log(valores[0])
 console.log(valores[1])
 console.log(valores[2])
 console.log(valores[3])
-*/
-
 for (let pos = 0; pos < valores.length; pos++) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
-
+} */
+for (var pos in valores) { // simplificando com for in 
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
