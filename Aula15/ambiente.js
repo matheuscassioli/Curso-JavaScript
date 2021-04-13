9ee574fb5d893234f8767fa479785c9728bf0d57
matheuscassioli <matheuscassioli@hotmail.com>
2021-04-13 18:01:37 -0300

    comandos importantes(arrays)

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -1,3 +1,9 @@
-    let num = [5, 8, 2, 9, 3]
+let num = [5, 8, 2, 9, 3]
+num.push(10) // adiciona 10 pós ultimo vetor
+console.log(num)
 
-    console.log(`Nosso vetor tem ${num.length} posições`)
+
+console.log(`Nosso vetor tem ${num.length} posições`) // .lenght sabe quantos vetores possui
+console.log(`Nosso vetor na posição 6 é o ${num[5]}`) // localiza elemento através da posição
+
+//num.sort() -- organiza vetores em ordem crescente
